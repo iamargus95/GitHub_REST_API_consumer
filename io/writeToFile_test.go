669dbec0a6cfd078bedc9a7186e7c9a05fbff4f2
iamargus95 <kamathsuraj95@gmail.com>
2021-09-08 16:14:12 +0530

    Broken testcase due to prams change

--- io/writeToFile_test.go
+++ io/writeToFile_test.go
@@ -2,13 +2,14 @@ package io
 
 import "testing"
 
-const filename = "test"
+func TestWriteToFile(t *testing.T) { //Fix Test
+	const filename = "test"
+	channel := make(chan []string)
+	var content = []string{"This is a test file.", "Delete this after t.Run."}
+	channel <- content
 
-var content = []string{"This is a test file.", "Delete this after t.Run."}
-
-func TestWriteToFile(t *testing.T) {
 	t.Run("test", func(t *testing.T) {
-		if err := WriteToFile(filename, content); (err != nil) != false {
+		if err := WriteToFile(filename, channel); (err != nil) != false {
 			t.Errorf("Test failed : %v", err)
 		}
 	})
