5f11239fcf194d932c294a7e169ce3ee3cec2ae2
iamargus95 <kamathsuraj95@gmail.com>
2021-09-09 12:45:40 +0530

    Added  concurrency flag to facilitate both sequencial and concurrent prog exec

--- main.go
+++ main.go
@@ -9,28 +9,52 @@ import (
 )
 
 func main() {
+
+	var con bool
+	flag.BoolVar(&con, "con", false, "Runs the application concurrently.")
+
 	flag.Parse()
 	usernames := flag.Args()
 
-	var wg sync.WaitGroup
-	dataToFile := make(chan []string)
+	if con {
 
-	for _, username := range usernames {
-		wg.Add(1)
-		go worker(username, dataToFile, &wg)
-		io.WriteToFile(username, dataToFile)
+		var wg sync.WaitGroup
+		dataToFile := make(chan []string, 1)
+
+		for _, username := range usernames {
+			wg.Add(1)
+			go worker(username, dataToFile, &wg)
+			go writeFile(username, dataToFile, &wg)
+		}
+		wg.Wait()
+
+	} else {
+
+		for _, username := range usernames {
+			userDetails := githubapi.GetUserData(username)
+			reposDetails := githubapi.GetReposData(username, userDetails.Public_repos)
+			userdata := strings.Split(userDetails.UserData(), ",")
+			repodata := reposDetails.RepoData()
+			accountData := append(userdata, repodata...)
+			io.WriteToFile(username, accountData)
+		}
 	}
 
-	wg.Wait()
 }
 
 func worker(username string, dataToFile chan []string, wg *sync.WaitGroup) {
 
-	defer wg.Done()
+	defer wg.Wait()
 	userDetails := githubapi.GetUserData(username)
 	reposDetails := githubapi.GetReposData(username, userDetails.Public_repos)
 	userdata := strings.Split(userDetails.UserData(), ",")
 	repodata := reposDetails.RepoData()
 	accountData := append(userdata, repodata...)
 	dataToFile <- accountData
+}
+
+func writeFile(username string, dataToFile chan []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	result := <-dataToFile
+	io.WriteToFile(username, result)
 }
