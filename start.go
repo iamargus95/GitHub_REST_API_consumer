9efbb2c222c83931b9f5c562c19ea5d508f3fd28
iamargus95 <kamathsuraj95@gmail.com>
2021-09-11 10:12:23 +0530

    Fixed concurrent File write issue

--- start.go
+++ start.go
@@ -3,7 +3,6 @@ package main
 import (
 	"iamargus95/fetchGithubData/githubapi"
 	"iamargus95/fetchGithubData/io"
-	"os"
 	"strings"
 	"sync"
 )
@@ -28,19 +27,20 @@ func concurrently(usernames []string) {
 	dataToFile := make(chan map[string][]string)
 
 	for _, username := range usernames {
-
-		wg.Add(1)
 		go fetch(username, dataToFile, &wg)
-		go writeFile(username, dataToFile, &wg)
 	}
 
+	wg.Add(len(usernames))
+
+	for _, username := range usernames {
+		go writeFile(username, dataToFile, &wg)
+	}
 	wg.Wait()
 
 }
 
 func fetch(username string, dataToFile chan map[string][]string, wg *sync.WaitGroup) {
 
-	defer wg.Wait()
 	userDetails := githubapi.GetUserData(username)
 	reposDetails := githubapi.GetReposData(username, userDetails.Public_repos)
 	userdata := strings.Split(userDetails.UserData(), ",")
@@ -50,7 +50,6 @@ func fetch(username string, dataToFile chan map[string][]string, wg *sync.WaitGr
 	dataToChannel := make(map[string][]string)
 	dataToChannel[username] = accountData
 	dataToFile <- dataToChannel
-
 }
 
 func writeFile(username string, dataToFile chan map[string][]string, wg *sync.WaitGroup) {
@@ -58,10 +57,5 @@ func writeFile(username string, dataToFile chan map[string][]string, wg *sync.Wa
 	result := <-dataToFile
 	value := result[username]
 	io.WriteToFile(username, value)
-	i, _ := os.Stat(username + ".txt")
-	if i.Size() != 0 {
-		defer wg.Done()
-	} else {
-		wg.Wait()
-	}
+	defer wg.Done()
 }
